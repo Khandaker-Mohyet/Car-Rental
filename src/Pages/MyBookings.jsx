d4f1d3371133a3905f11cde2e8574a52e14fbede
Khandaker Mohyet <mohyet0007@gmail.com>
2024-12-25 02:30:11 +0600

    booking dealte

--- src/Pages/MyBookings.jsx
+++ src/Pages/MyBookings.jsx
@@ -1,5 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
+import Swal from "sweetalert2";
 
 
 const MyBookings = () => {
@@ -13,9 +14,53 @@ const MyBookings = () => {
         console.log(data)
         setBookes(data)
       })
-
-
   }, [user?.email])
+
+
+  const handelCarDelete = (id) => {
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          fetch(`http://localhost:5000/booking/${id}`, {
+            method: "DELETE",
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+              if (data.deletedCount > 0) {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Your car has been deleted.",
+                  icon: "success",
+                });
+  
+                // Update the cars state after deletion
+                const remainingCars = bookes.filter((car) => car._id !== id);
+                setBookes(remainingCars);
+              }
+            })
+            .catch((error) => {
+              console.error("Error deleting car:", error);
+              Swal.fire({
+                title: "Error!",
+                text: "Failed to delete the car. Please try again.",
+                icon: "error",
+              });
+            });
+        }
+      });
+    };
+
+
+
+
   return (
     <div>
       This is My booking page {bookes.length}
@@ -67,7 +112,7 @@ const MyBookings = () => {
               <td>Purple</td>
               <th className="">
                 <button className="mr-2 btn btn-warning btn-xs">Update</button>
-                <button className="btn btn-error btn-xs">Delate</button>
+                <button onClick={() => handelCarDelete(book._id)} className="btn btn-error btn-xs">Delate</button>
               </th>
             </tr>)
             }
