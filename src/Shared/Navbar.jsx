40a1ffc5d925e20b97b35fe3878d555036f0b055
Khandaker Mohyet <mohyet0007@gmail.com>
2025-02-12 19:38:15 +0600

    fixed

--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -7,7 +7,7 @@ import logo from '../assets/Logo.png'
 const Navbar = () => {
   const { user, singInOut } = useContext(AuthContext)
   return (
-    <div className="navbar bg-base-100 md:px-8 mx-auto top-0 ">
+    <div className="navbar bg-base-100 md:px-8 mx-auto top-0 fixed ">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
