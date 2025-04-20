f262ab97f49207baf5ea5b5fe7b5c7e9d7af2c63
Khandaker-Mohyet <mohyet008@gmail.com>
2025-04-21 01:14:43 +0600

    auth theme update

--- src/Layout/AuthLayout.jsx
+++ src/Layout/AuthLayout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Shared/Navbar";
+import { useThemeStore } from "../Store/useThemStore";
 
 
 const AuthLayout = () => {
+  const {theme}= useThemeStore()
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar></Navbar>
       <Outlet></Outlet>
     </div>
